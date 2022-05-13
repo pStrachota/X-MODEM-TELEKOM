b53edc9e5077540adc82883f63e8e52c465348aa
pStrachota <p.strachota52@gmail.com>
2022-05-13 20:15:48 +0200

    add checksum and signals classes in model

---
+++ X_MODEM/Model/Checksum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class Checksum
+    {
+        public static byte AlgebraicChecksum(byte[] bytes)
+        {
+            int sum = 0;
+            foreach (var addend in bytes)
+            {
+                sum += addend;
+                sum %= 256;
+            }
+            return (byte)sum;
+        }
+
+        public static byte[] CreateCheckSumCRC(byte[] package)
+        {
+            int checksumCRC = 0;
+            foreach (byte _byte in package)
+            {
+                checksumCRC = checksumCRC ^ _byte << 8;
+                for (int i = 0; i < 8; i++)
+                {
+                    if (Convert.ToBoolean(checksumCRC & 0x8000))
+                        checksumCRC = (checksumCRC >> 1) ^ 0x1021;
+                    else
+                        checksumCRC = checksumCRC >> 1;
+                }
+            }
+            return BitConverter.GetBytes(checksumCRC);
+        }
+    }
+}
---
+++ X_MODEM/Model/Signals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHARP_PW_PROJECT.Model
+{
+    static class Signals
+    {
+        public const int SOH = 1;
+        public const int EOT = 4;
+        public const int ACK = 6;
+        public const int NAK = 21;
+        public const int SUB = 26;
+        public const int C = 67;
+    }
+}
